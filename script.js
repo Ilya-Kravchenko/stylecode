f022431b316ee51c5c444d4f47691d5056151d1f
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-01-07 15:08:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+function pow(x, n) {
+    let result = 1;
+
+    for (let i =0; i < n; i++) {
+        result *=x;
+    }
+
+    return result;
+}
+
+    let x = prompt ('x?', '');
+    let n = prompt ('n?', '');
+
+    if (n < 0) {
+     alert(`Power ${n} is not supported,
+     please enter a non-negative integer number`);
+    } else {
+     alert( pow(x, n) );
+    }
+
+    if (n < 0) {
+     alert(`Степень ${n} не поддерживаеться`);
+    }
+
+    let src =
+    Рабочая группа TC39 организации Ecma international -
+    это группа JavaScript-разработчиков, теоретиков и авторов движков JavaScript,
+    которые вместе с сообществом занимаються поддержкой и развитием языка JavaScript.
+    ;
+
+
+
+if (
+  id === 123 &&
+  moonPhase === 'Waning Gibbous' &&
+  zodiacSignal === 'Libra'
+) {
+  letTheSorceryBegin ();
+}
+
+
+
+function pow(x, n) {
+   let result = 1;
+   
+   for (let i = 0; i < n; i++) {
+      result *= x;
+   }
+
+   return result;
+}
+
+
+
+function pow(x, n) {
+   let result = 1;
+
+   for (let i = 0; i < n; i++) {
+    result *= x; 
+   }
+
+    return result;
+   }
+
+   let x = prompt ('x?', '');
+   let n = prompt ('n?', '');
+
+   if (n <= 0) {
+    alert(`Степень ${n} не поддерживаеться,
+     введите целую степень, большую 0`);
+   } else {
+       alert ( pow(x, n) );
+   }
+
